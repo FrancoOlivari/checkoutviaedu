857482a381a1e514133e8688c7044ffc3ba07c31
FrancoOlivari <francoolivari97@gmail.com>
2017-01-20 15:50:27 -0300

    Fixes Various

--- js/pills.js
+++ js/pills.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
-    $(".toggle-content").hide();
-    $(".toggle-title").click(function() {
-        $(this).next(".toggle-content").slideToggle("normal");
-        $(this).toggleClass('active');
-    });
+	$('.accordion').find('.accordion-toggle').click(function() {
+		$(this).next().slideToggle('600');
+		$(".accordion-content").not($(this).next()).slideUp('600');
+	});
+	$('.accordion-toggle').on('click', function() {
+		$(this).toggleClass('active').siblings().removeClass('active');
+	});
 });
+	
